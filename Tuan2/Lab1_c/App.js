a0eb3d0c67faf7865f98e0bb7f94e0c8116aaf7f
Tram <mavutotram123@gmail.com>
2022-09-13 11:55:52 +0700

    Lab1_c

---
+++ Tuan2/Lab1_c/App.js
@@ -0,0 +1,83 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import Rectangle_1 from '../Lab1_c/assets/img/Rectangle1.png'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <LinearGradient 
+      colors={['#D1F4F6','#E5F4F5','#E5F4F5','#E5F4F5','transparent']}
+      style={styles.background}>
+        <View style={{flex:3, alignItems:'center', justifyContent:'center'}}>
+          <Text style={styles.Logo}>CODE</Text>
+        </View>
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+          <Text style={[styles.Text, {fontSize:28, paddingBottom:10}]}>VERIFICATION</Text>
+          <Text style={[styles.Text, {fontSize:20}]}>Enter ontime password sent on ++849092605798</Text>
+        </View>
+        <View style={{flex:1, flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+          <TextInput style={styles.Input} numberOfLines={1} maxLength={1}></TextInput>
+          <TextInput style={styles.Input} numberOfLines={1} maxLength={1}></TextInput>
+          <TextInput style={styles.Input} numberOfLines={1} maxLength={1}></TextInput>
+          <TextInput style={styles.Input} numberOfLines={1} maxLength={1}></TextInput>
+          <TextInput style={styles.Input} numberOfLines={1} maxLength={1}></TextInput>
+          {/* <Image source={Rectangle_1}></Image>
+          <Image source={Rectangle_1}></Image>
+          <Image source={Rectangle_1}></Image>
+          <Image source={Rectangle_1}></Image>
+          <Image source={Rectangle_1}></Image> */}
+        </View>
+        <View style={{flex:1}}>
+          <TouchableOpacity>
+            <Text style={styles.button}>VERIFY CODE</Text>
+          </TouchableOpacity>
+        </View>
+      </LinearGradient>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent:'center',
+    backgroundColor: '#00CCF9',
+    height:'100%',
+  },
+  background:{
+    left:0,
+    right:0,
+    top:0,
+    height:'100%',
+  },
+  Logo:{
+    color:'black',
+    fontSize:60,
+    fontWeight:'bold',
+  },
+  Text:{
+    color:'black',
+    fontWeight:'bold',
+    textAlign:'center',
+  },
+  button:{
+    backgroundColor:'#E3C000',
+    fontSize:25,
+    fontWeight:'bold',
+    padding:10,
+    textAlign:'center',
+    marginLeft:30,
+    marginRight:30,
+  },
+  Input:{
+    borderWidth:2,
+    fontSize:30,
+    textAlign:'center',
+    borderColor:'black',
+    height:65,
+    width:65,
+  }
+});
